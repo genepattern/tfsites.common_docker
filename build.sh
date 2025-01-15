a8e1376f1da008e90252641f8520824b6f427183
liefeld <liefeld@broadinstitute.org>
2025-01-15 08:22:22 -0800

    Updated to add 05-mapTfDnaInteractions

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 # to build on my m4 mac
 
-docker buildx build --tag genepattern/tfsites:0.11 -o type=image --platform=linux/arm64,linux/amd64 --push  .
+docker buildx build --tag genepattern/tfsites:0.13 -o type=image --platform=linux/arm64,linux/amd64 --push  .
 

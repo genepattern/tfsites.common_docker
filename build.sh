93520e3e3c9533b021c9d6ae99d4f9d11f095109
liefeld <liefeld@broadinstitute.org>
2025-04-25 11:46:01 -0700

    Updated code from farley lab

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 # to build on my m4 mac
 
-docker buildx build --tag genepattern/tfsites:15.7 -f Dockerfile_incremental -o type=image --platform=linux/amd64,linux/arm64 --push  .
+docker buildx build --tag genepattern/tfsites:15.8 -f Dockerfile_incremental -o type=image --platform=linux/amd64,linux/arm64 --push  .
 

b3165b147eddc882059d779b997fe2cd9ee789b1
ted Liefeld <liefeld@broadinstitute.org>
2025-09-30 14:17:04 -0700

    Working version of generateMotifDatabase before visualizeTFSitesOnSequences

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 # to build on my m4 mac
 
-docker buildx build --tag genepattern/tfsites:17.0 -f Dockerfile -o type=image --platform=linux/amd64 --push  .
+docker buildx build --tag genepattern/tfsites:17.0.2 -f Dockerfile -o type=image --platform=linux/amd64 --push  .
 

5859546d9a1a9c28f05fcafb7721ec80164cd402
Ted Liefeld <liefeld@broadinstitute.org>
2026-04-06 07:07:44 -0700

    Update for Normalize multiple normalization methods

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 # to build on my m4 mac
 
-docker buildx build --tag genepattern/tfsites:17.1.5 -f Dockerfile -o type=image --platform=linux/amd64 --push  .
+docker buildx build --tag genepattern/tfsites:18.0 -f Dockerfile -o type=image --platform=linux/amd64 --push  .
 
